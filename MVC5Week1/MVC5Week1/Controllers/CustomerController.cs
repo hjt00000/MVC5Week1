d447d5c588f1c2c2d7a2a6a41b1a25d15a063c9f
ChiaChu Huang <hjt00000@hotmail.com>
2016-03-10 00:13:12 +0800

    客戶資料 CRUD 完成

---
+++ MVC5Week1/MVC5Week1/Controllers/CustomerController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Week1.Models;
+using System.Data.Entity;
+
+namespace MVC5Week1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var data = db.客戶資料.Where(p => p.是否已刪除 == false);
+
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(客戶資料 CustomerData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶資料.Add(CustomerData);
+                db.SaveChanges();
+            }
+            return RedirectToAction("客戶資料");
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var data = db.客戶資料.FirstOrDefault(p => p.Id == Id);
+            return View(data);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var data = db.客戶資料.Find(Id);
+            if(data == null)
+            {
+                return HttpNotFound();
+            }
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(客戶資料 CustomerData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(CustomerData).State = EntityState.Modified;
+                db.SaveChanges();
+                
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var data = db.客戶資料.Find(id);
+            data.是否已刪除 = true;
+            db.Entry(data).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
